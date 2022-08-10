be992821873737e52c0310ba27c7302a4d33a72e
EduardoReisUX <ducabruno09@hotmail.com>
2022-08-09 20:52:34 -0400

    feat: add 400 response if category already exists

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -28,6 +28,10 @@ class CategoriesRepository {
   list(): Category[] {
     return this.categories;
   }
+
+  findByName(name: string): Category {
+    return this.categories.find((category) => category.name === name);
+  }
 }
 
 export { CategoriesRepository };
