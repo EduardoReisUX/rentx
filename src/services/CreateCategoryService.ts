2925d0d895354b9827f5705d3494d7cbd2db96c3
EduardoReisUX <ducabruno09@hotmail.com>
2022-08-11 12:51:43 -0400

    refactor: add ICategoriesRepository applying LSP from SOLID

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
 
 interface IRequest {
   name: string;
@@ -6,7 +6,7 @@ interface IRequest {
 }
 
 class CreateCategoryService {
-  constructor(private categoriesRepository: CategoriesRepository) {}
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
   execute({ name, description }: IRequest): void {
     const categoryAlreadyExists = this.categoriesRepository.findByName(name);
@@ -20,10 +20,3 @@ class CreateCategoryService {
 }
 
 export { CreateCategoryService };
-
-/* 
-[x] - Definir o tipo de retorno
-[x] - Alterar o retorno de erro
-[x] - Acessar o repositorio
-[x] - Retornar algo
-*/
