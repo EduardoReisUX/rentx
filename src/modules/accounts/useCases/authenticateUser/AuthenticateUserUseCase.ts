aefc4f53f4baea56275588759d7e21e862589414
EduardoReisUX <ducabruno09@hotmail.com>
2022-08-29 21:47:33 -0400

    feat: add user session with jsonwebtoken

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,59 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+export class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const token = sign(
+      email,
+      "94e2c128c93bec67fe599c3c1aafbb0fbec1aab90519c1443fe46345ce156768",
+      {
+        subject: user.id, // autor do request -> sempre o id
+        expiresIn: "1d", // 1 dia
+      }
+    );
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { authenticateRoutes } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
 import { usersRouters } from "./user.routes";
@@ -8,3 +9,4 @@ export const router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRouters);
+router.use(authenticateRoutes);
