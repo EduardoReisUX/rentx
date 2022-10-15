8090a62c2113b45cf159638d3aa9787bb68f65d6
Eduardo Fernandes <ducabruno09@hotmail.com>
2022-10-15 16:03:12 -0400

    feat: add provider for datejs

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,10 +1,7 @@
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
-
-dayjs.extend(utc);
 
 interface IRequest {
   user_id: string;
@@ -13,7 +10,10 @@ interface IRequest {
 }
 
 export class CreateRentalUseCase {
-  constructor(private rentalsRepository: IRentalsRepository) {}
+  constructor(
+    private rentalsRepository: IRentalsRepository,
+    private dateProvider: IDateProvider
+  ) {}
   async execute({
     user_id,
     car_id,
@@ -21,7 +21,6 @@ export class CreateRentalUseCase {
   }: IRequest): Promise<Rental> {
     const minimumHour = 24;
 
-    // Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo carro.
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
       car_id
     );
@@ -30,7 +29,6 @@ export class CreateRentalUseCase {
       throw new AppError("Car is unavailable");
     }
 
-    // Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo usuário.
     const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
       user_id
     );
@@ -39,15 +37,12 @@ export class CreateRentalUseCase {
       throw new AppError("There's a rental in progress for user!");
     }
 
-    // O aluguel deve ter duração mínima de 24 horas.
-    const expectedReturnDateFormat = dayjs(expected_return_date)
-      .utc()
-      .local()
-      .format();
+    const dateNow = this.dateProvider.dateNow();
 
-    const dateNow = dayjs().utc().local().format();
-
-    const compare = dayjs(expectedReturnDateFormat).diff(dateNow, "hours");
+    const compare = this.dateProvider.compareInHours(
+      dateNow,
+      expected_return_date
+    );
 
     if (compare < minimumHour) {
       throw new AppError("Invalid return time!");
